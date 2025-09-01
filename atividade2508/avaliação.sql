bb337094adedb11f99bfb9120d00ec39939d8b61
MariaEduarda-Nepo <mariaeduardan995@gmail.com>
2025-09-01 16:17:24 -0300

    ...........................

--- atividade2508/avaliação.sql
+++ atividade2508/avaliação.sql
@@ -45,3 +45,39 @@ CREATE TABLE Fornecimento (
     FOREIGN KEY (Pcod) REFERENCES Peca(Pcodigo),
     FOREIGN KEY (PRcod) REFERENCES Projeto(PRcod)
 );
+
+-- Criar tabela Cidade
+CREATE TABLE Cidade (
+    Ccod INT PRIMARY KEY,
+    Cnome VARCHAR(100) NOT NULL,
+    UF CHAR(2) NOT NULL
+);
+
+-- Alterar Fornecedor
+ALTER TABLE Fornecedor
+    ADD Fone VARCHAR(20),
+    ADD Ccod INT,
+    ADD CONSTRAINT fk_fornecedor_cidade FOREIGN KEY (Ccod) REFERENCES Cidade(Ccod);
+
+-- Alterar Peca
+ALTER TABLE Peca
+    ADD Ccod INT,
+    ADD CONSTRAINT fk_peca_cidade FOREIGN KEY (Ccod) REFERENCES Cidade(Ccod);
+
+-- Alterar Projeto
+ALTER TABLE Projeto
+    DROP COLUMN Cidade,
+    DROP FOREIGN KEY projeto_ibfk_1, -- exclui FK antiga de Instituicao
+    DROP COLUMN Icod,
+    ADD Ccod INT,
+    ADD CONSTRAINT fk_projeto_cidade FOREIGN KEY (Ccod) REFERENCES Cidade(Ccod);
+
+-- Excluir tabela Instituicao
+DROP TABLE Instituicao;
+
+-- Índices para acelerar consultas
+CREATE INDEX idx_fornecedor_nome ON Fornecedor(Fnome);
+CREATE INDEX idx_peca_nome ON Peca(Pnome);
+CREATE INDEX idx_projeto_nome ON Projeto(PRnome);
+CREATE INDEX idx_cidade_nome ON Cidade(Cnome);
+
